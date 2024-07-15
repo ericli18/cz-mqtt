e74ed9c7b5c0abfdb8c104c55d498a809aa799ac
Eric Li <eric.li.8736@gmail.com>
2024-07-15 01:32:55 -0500

    Pollclient can send and receive messages

--- pollclient.c
+++ pollclient.c
@@ -8,17 +8,15 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT "8080"
+#define PORT "3490"
 #define MAXBUFSIZE 100
 
-void* getaddr(struct sockaddr* sa)
-{
-  if(sa->sa_family == AF_INET)
-  {
-    return &(((struct sockaddr_in *)sa) -> sin_addr);
+void *getaddr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
   }
 
-  return &(((struct sockaddr_in6*)sa) -> sin6_addr);
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
 int main() {
@@ -30,7 +28,7 @@ int main() {
   int numBytes;
   char s[INET6_ADDRSTRLEN];
 
-  struct pollfd pfds[1];
+  struct pollfd pfds[2];
 
   memset(&hints, 0, sizeof(hints));
   hints.ai_socktype = SOCK_STREAM;
@@ -71,14 +69,42 @@ int main() {
   pfds[0].fd = sockfd;
   pfds[0].events = POLLIN | POLLOUT;
 
-  if ((numBytes = recv(sockfd, buf, MAXBUFSIZE - 1, 0)) == -1) {
-    perror("recv: ");
-    exit(1);
+  pfds[1].fd = STDIN_FILENO;
+  pfds[1].events = POLLIN;
+
+  for (;;) {
+    int numEvents = poll(pfds, 2, -1);
+    if (numEvents == -1) {
+      fprintf(stderr, "there was a problem when sending or receiving data\n");
+    }
+
+    if (pfds[0].revents & POLLIN) {
+      if ((numBytes = recv(sockfd, buf, MAXBUFSIZE - 1, 0)) == -1) {
+        perror("recv: ");
+        exit(1);
+      }
+
+      buf[numBytes] = '\0';
+
+      printf("recieved: %s\n", buf);
+    }
+
+    if (pfds[1].revents & POLLIN) {
+      fgets(buf, MAXBUFSIZE, stdin);
+      buf[strcspn(buf, "\n")] = 0;
+
+      if (strcmp(buf, "quit") == 0) {
+        printf("Exiting...\n");
+        break;
+      }
+
+      if (send(sockfd, buf, strlen(buf), 0) == -1) {
+        perror("send");
+      } else {
+        printf("Sent: %s\n", buf);
+      }
+    }
   }
-
-  buf[numBytes] = '\0';
-
-  printf("recieved: %s\n", buf);
 
   close(sockfd);
 
